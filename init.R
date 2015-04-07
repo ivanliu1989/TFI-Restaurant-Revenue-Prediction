8c81c1a821be678f501cbef9612f5d3624607016
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-04-07 21:58:27 +1000

    init

--- init.R
+++ init.R
@@ -1,7 +1,12 @@
 setwd("Documents/TFI-Restaurant-Revenue-Prediction")
 rm(list = ls());gc()
-require(data.table);
+require(data.table);require(caret)
 
-train_df <- fread('data/train.csv')
-test_df <- fread('data/test.csv')
-submission <- fread('data/sampleSubmission.csv')
+train_df <- data.frame(fread('data/train.csv'))
+test_df <- data.frame(fread('data/test.csv'))
+submission <- data.frame(fread('data/sampleSubmission.csv'))
+
+head(train_df);head(test_df);head(submission)
+str(train_df)
+
+fit <- train(revenue ~ ., data=train_df[,-1], method = 'rf')
